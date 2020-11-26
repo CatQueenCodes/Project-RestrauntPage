75f5c6ec78f06a8ba7cb4f6812d71eced3999cc7
Grace Schwartz <grace.schwar@gmail.com>
2020-11-25 21:43:11 -0500

    https://catqueencodes.github.io/img/cat.jpg

--- src/Modules/about.js
+++ src/Modules/about.js
@@ -2,7 +2,7 @@ const about = {
     renderAreaId: 'contContainer',
     render: (location) => {
         const photo = document.createElement('img');
-        photo.src = ('../img/cat.jpg');
+        photo.src = 'https://catqueencodes.github.io/img/cat.jpg';
         location.textContent = 'Please excuse any cat hair in your food and this basic design. This project is meant to be an introduction to webpack, and I did not spend much time on CSS. Focused on DRY, Objects, and clean code.';
         location.appendChild(photo);
     }
